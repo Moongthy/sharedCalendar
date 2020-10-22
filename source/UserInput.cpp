899887beab26aa561ed55a4d83b2cfd70f357644
Moongthy <cmk5187@konkuk.ac.kr>
2020-10-22 19:56:20 +0900

    Date, Schedule, User type 정의 될 때 까지 아무것도 못함 ㅋㅋ

--- source/UserInput.cpp
+++ source/UserInput.cpp
@@ -16,15 +16,8 @@ bool UserInput::encodingCheck(){
     for(int i = (int)'가'; i <= (int)'힣'; ++i) s += (char)i;
 
     for(char u : userInput){
-        bool check = false;
-        // 한 글자씩 비교, 제대로 인코딩된 문자입력했을시 return false까지 가지 못한다.
-        for(char e : s)
-            if(e == u) {
-                check = true;
-                continue;
-            }
-        if(!check) return false;
-    }
+        if(s.find(u) != string::npos)
+            return false;
     return true;
 }
 
