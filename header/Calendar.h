68efc99790db60a07802a2d3d432a6e9a9e159a7
yeseull <69413452+yeseull@users.noreply.github.com>
2020-10-22 14:52:29 +0900

    Update Calendar.h

--- header/Calendar.h
+++ header/Calendar.h
@@ -19,7 +19,7 @@ using std::string;
 template <typename S, typename U, typename D> class Calendar{
 protected:
     // 캘린더 id
-    string calendarID; //int?? string??
+    string calendarID; //
 
     // 만든사람
     U administrator;
