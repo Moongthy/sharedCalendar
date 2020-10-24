c387928b7f944b66058f9d310f09e873193bdf5d
Moongthy <cmk5187@konkuk.ac.kr>
2020-10-24 21:56:18 +0900

    check.h modify

--- header/input/check.h
+++ header/input/check.h
@@ -6,8 +6,11 @@
 #include<string>
 using std::string;
 
-
-
+#define _NORMAL 0
+#define _SCNAME 1
+#define _PW 2
+#define _ACCEPTABLE 3
+#define _DATE 4
 
 class check{
 public:
@@ -54,41 +57,32 @@ public:
 
     bool totalCheck(string& s, int n, int upper){
         // 일반 메뉴 선택 입력
-        if(n == 0){
+        if(n == _NORMAL){
             if(s.size() != 1) return false;
             return numberCheck(s, upper);
         }
 
         // 캘이름
-        if(n == 1){
+        if(n == _SCNAME){
             if(s.size() < 2 || s.size() > 10) return false;
             return encodingCheck(s);
         }
 
         // 캘 비번
-        if(n == 2){
+        if(n == _PW){
             if(s.size() < 2 || s.size() > 15) return false;
             return encodingCheck(s);
         }
 
         // 입장인원
-        if(n == 3){
+        if(n == _ACCEPTABLE){
             if(s.size() != 1) return false;
             return numberCheck(s, 5);
         }
 
         // 유효기간
-        if(n == 4){
+        if(n == _DATE){
             return true;
-            // if(s.size() == 6) return dateCheck(s);
-            // if(s.size() == 8){
-            //     if((!(s[2] == '-' && s[5] == '-') || (s[2] == '/' && s[5] == '/')))
-            //         return false;
-            //     string tmp = s.substr(0, 1) + s.substr(3, 4) + s.substr(6, 7);
-            //     s = tmp;
-            //     return dateCheck(s);
-            // }
-            // return false;
         }
 
         return true; 
